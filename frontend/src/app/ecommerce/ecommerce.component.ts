49c63244f6db0283adfa051bee8fac224dee71d3
ZeraiGR <kirillgr15@mail.ru>
2025-08-05 17:35:45 +0300

    fix(build): make work angular production build

--- frontend/src/app/ecommerce/ecommerce.component.ts
+++ frontend/src/app/ecommerce/ecommerce.component.ts
@@ -1,44 +1,42 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {ProductsComponent} from "./products/products.component";
-import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
-import {OrdersComponent} from "./orders/orders.component";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ProductsComponent } from "./products/products.component";
+import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
+import { OrdersComponent } from "./orders/orders.component";
 
 @Component({
-    selector: 'app-ecommerce',
-    templateUrl: './ecommerce.component.html',
-    styleUrls: ['./ecommerce.component.css']
+  selector: "app-ecommerce",
+  templateUrl: "./ecommerce.component.html",
+  styleUrls: ["./ecommerce.component.css"],
 })
 export class EcommerceComponent implements OnInit {
-    private collapsed = true;
-    orderFinished = false;
+  collapsed = true;
+  orderFinished = false;
 
-    @ViewChild('productsC')
-    productsC: ProductsComponent;
+  @ViewChild("productsC")
+  productsC: ProductsComponent;
 
-    @ViewChild('shoppingCartC')
-    shoppingCartC: ShoppingCartComponent;
+  @ViewChild("shoppingCartC")
+  shoppingCartC: ShoppingCartComponent;
 
-    @ViewChild('ordersC')
-    ordersC: OrdersComponent;
+  @ViewChild("ordersC")
+  ordersC: OrdersComponent;
 
-    constructor() {
-    }
+  constructor() {}
 
-    ngOnInit() {
-    }
+  ngOnInit() {}
 
-    toggleCollapsed(): void {
-        this.collapsed = !this.collapsed;
-    }
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
 
-    finishOrder(orderFinished: boolean) {
-        this.orderFinished = orderFinished;
-    }
+  finishOrder(orderFinished: boolean) {
+    this.orderFinished = orderFinished;
+  }
 
-    reset() {
-        this.orderFinished = false;
-        this.productsC.reset();
-        this.shoppingCartC.reset();
-        this.ordersC.paid = false;
-    }
+  reset() {
+    this.orderFinished = false;
+    this.productsC.reset();
+    this.shoppingCartC.reset();
+    this.ordersC.paid = false;
+  }
 }
